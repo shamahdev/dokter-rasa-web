d2d784cc28352b807fc345e47a52f291a461ce50
Shaddam Amru Hasibuan <shaddam.a.h@gmail.com>
2020-08-30 00:26:01 +0700

    Add E2E Test Spec

---
+++ e2e/Add_And_Remove_Restaurant_Bookmark.spec.js
@@ -0,0 +1,33 @@
+const assert = require('assert');
+
+Feature('Add And Remove Restaurant Bookmark');
+Before((I) => {
+    I.amOnPage('/#/bookmark');
+});
+
+Scenario('Showing empty bookmark', (I) => {
+    I.see('Belum ada restoran yang ditambahkan ke bookmark', 'p.center.mh-auto');
+});
+
+Scenario('Add and remove one restaurant bookmark', async (I) => {
+    I.click(locate('a.btn.primary').first());
+
+    I.seeElement('button[data-bookmark]');
+    const firstRestaurantCard = locate('.card-text h3').first();
+    const firstRestaurantName = await I.grabTextFrom(firstRestaurantCard);
+    I.click(locate('button[data-bookmark]').first());
+
+    setTimeout(() => {
+        I.amOnPage('/#/bookmark');
+    }, 100);
+    I.seeElement('restaurant-card');
+    const bookmarkedRestaurantCard = locate('.card-text h3').first();
+    const bookmarkedRestaurantName = await I.grabTextFrom(bookmarkedRestaurantCard);
+   
+    assert.strictEqual(bookmarkedRestaurantName, firstRestaurantName);
+
+    I.see('Lihat detail', 'a.trigger')
+    I.click(locate('a.trigger').first());
+    I.pressKey('B'); /* Press bookmark button with accessibility key */
+    I.dontSeeElement('button[data-bookmark]');
+});
---
+++ e2e/Add_Restaurant_Review.spec.js
@@ -0,0 +1,1 @@
+Feature('Add Restaurant Review');
